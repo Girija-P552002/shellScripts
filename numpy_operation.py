174d224487e0d5c0dd90a4a11e90ffcdc252d001
girija <anusha2002girija@gmail.com>
2024-08-05 15:08:28 +0530

    perform numpy operations

---
+++ numpy_operation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+array = np.array([1, 2, 3, 4, 5])
+
+# Perform some basic operations
+print("Array:", array)
+print("Sum:", np.sum(array))
+print("Mean:", np.mean(array))
+print("Max:", np.max(array))
+print("Min:", np.min(array))
+
+# Create a 2D array (matrix)
+matrix = np.array([[1, 2, 3], [4, 5, 6]])
+
+# Access elements
+print("Element at (1, 2):", matrix[1, 2])
